97f14c442bd792edd4a53498c75b5dcf0c28bd53
alisafwat <alisafwat11@gmail.com>
2025-04-15 19:09:37 +0200

    add: splash screen content

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.newsapp
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,6 +32,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsAppTheme {
+                Handler(mainLooper).postDelayed({},2500)
                 SplashContent()
             }
         }
@@ -34,11 +43,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SplashContent() {
     Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
             .paint(painterResource(R.drawable.pattern), contentScale = ContentScale.FillBounds)
     ) {
-        Image(painterResource(id = R.drawable.logo), contentDescription = "Logo")
+        Spacer(modifier = Modifier.weight(2F))
+        Image(
+            painterResource(id = R.drawable.logo),
+            contentDescription = stringResource(R.string.logo),
+            modifier = Modifier.fillMaxHeight(0.25F),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.weight(1F))
+        Image(
+            painterResource(id = R.drawable.supervised),
+            contentDescription = stringResource(R.string.supervised),
+            Modifier
+                .fillMaxHeight(.25F),
+            contentScale = ContentScale.Crop
+        )
 
     }
 }
